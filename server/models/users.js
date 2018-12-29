7e1c5ad1ae945e210a3b8fc894bbcabee869b9f7
Kian Eliasi <kian.elbo@gmail.com>
2018-12-29 14:55:34 +0330

    db created

---
+++ server/models/users.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema, 'users');
+
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const config = require('../../config/database');
+
+mongoose.connect(config.database, err => {
+  if (err)
+    console.log('ERROR! ' + err);
+  else
+    console.log('connected to mongodb');
+});
 
 router.get('/', (req, res) => {
   res.send('From API route')
