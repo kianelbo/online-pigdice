6839d950ec2709c8b677921527ee69ad0919c5bb
Kian Eliasi <kian.elbo@gmail.com>
2018-12-29 14:38:29 +0330

    routes created

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const api = require('./routes/api');
+
 const PORT = 3000;
 
 const app = express();
 
 app.use(bodyParser.json());
 
+app.use('/api', api);
 app.get('/', function (req, res) {
   res.send('Hello');
 });
