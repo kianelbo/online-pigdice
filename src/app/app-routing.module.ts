8849832016c3e8b4eac246533e70bc3c5942f4bc
Kian Eliasi <kian.elbo@gmail.com>
2018-12-29 16:57:30 +0330

    angular base setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {EventsComponent} from './events/events.component';
+import {LoginComponent} from './login/login.component';
+import {SpecialEventsComponent} from './special-events/special-events.component';
+import {RegisterComponent} from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/events', pathMatch: 'full' },
+  { path: 'events', component: EventsComponent },
+  { path: 'special', component: SpecialEventsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
