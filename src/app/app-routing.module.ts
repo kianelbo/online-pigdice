4434b2356facb23876a59338782d664e1a2c9468
Kian Eliasi <kian.elbo@gmail.com>
2019-01-05 01:08:23 +0330

    tidy up and bg img

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { IndexComponent } from './index/index.component';
-import { EventsComponent } from './events/events.component';
 import { LoginComponent } from './login/login.component';
-import { SpecialEventsComponent } from './special-events/special-events.component';
 import { RegisterComponent } from './register/register.component';
 import { CommentsConfirmComponent } from './comments-confirm/comments-confirm.component';
 import { DesignGameComponent } from './design-game/design-game.component';
@@ -23,8 +21,6 @@ import { AdminGuard } from './guards/admin.guard';
 const routes: Routes = [
   { path: '', redirectTo: '/index', pathMatch: 'full' },
   { path: 'index', component: IndexComponent },
-  { path: 'events', component: EventsComponent },
-  { path: 'special', component: SpecialEventsComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'admin-page', component: CommentsConfirmComponent, canActivate: [AdminGuard] },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,6 @@ import { IndexComponent } from './index/index.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { CommentsConfirmComponent } from './comments-confirm/comments-confirm.component';
-import { EventsComponent } from './events/events.component';
-import { SpecialEventsComponent } from './special-events/special-events.component';
 import { DesignGameComponent } from './design-game/design-game.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
@@ -24,7 +22,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
 
 import { AuthService } from './services/auth.service';
 import { TokenInterceptorService } from './services/token-interceptor.service';
-import { EventService } from './services/event.service';
 import { FriendshipService } from './services/friendship.service';
 import { CommentService } from './services/comment.service';
 import { AuthGuard } from './guards/auth.guard';
@@ -36,8 +33,6 @@ import { AdminGuard } from './guards/admin.guard';
     IndexComponent,
     RegisterComponent,
     LoginComponent,
-    EventsComponent,
-    SpecialEventsComponent,
     CommentsConfirmComponent,
     DesignGameComponent,
     LeaderboardComponent,
@@ -55,7 +50,7 @@ import { AdminGuard } from './guards/admin.guard';
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [DatePipe, AuthService, FriendshipService, EventService, CommentService,
+  providers: [DatePipe, AuthService, FriendshipService, CommentService,
     {provide: HTTP_INTERCEPTORS, useClass: TokenInterceptorService, multi: true},
     AuthGuard, AdminGuard],
   bootstrap: [AppComponent]
