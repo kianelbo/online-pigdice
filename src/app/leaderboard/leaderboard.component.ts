55927871893608f7dd31f6bbfebae6494383dba2
Kian Eliasi <kian.elbo@gmail.com>
2019-01-18 13:14:10 +0330

    leaderboard additional sorts

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -14,7 +14,19 @@ export class LeaderboardComponent implements OnInit {
 
   ngOnInit() {
     this.authService.getAllUsers().subscribe(
-      res => this.users = res,
+      res => {
+        this.users = res;
+        this.users.forEach(u => {
+          u.designedGames = u.createdGames.length;
+          u.gamesRatings = 0;
+          u.gamesTotalPlayed = 0;
+          u.createdGames.forEach(g => {
+            u.gamesRatings += g.avgRating;
+            u.gamesTotalPlayed += g.totalPlayed;
+          });
+          u.gamesRatings /= u.designedGames;
+        });
+      },
       err => console.error(err));
     this.sortByGames();
   }
@@ -32,6 +44,21 @@ export class LeaderboardComponent implements OnInit {
   sortByRating() {
     this.users.sort((a, b) => (a.avgRating < b.avgRating) ? 1 : ((b.avgRating < a.avgRating) ? -1 : 0));
     this.sortMethod = 3;
+  }
+
+  sortByDesignedGames() {
+    this.users.sort((a, b) => (a.designedGames < b.designedGames) ? 1 : ((b.designedGames < a.designedGames) ? -1 : 0));
+    this.sortMethod = 4;
+  }
+
+  sortByGamesRatings() {
+    this.users.sort((a, b) => (a.gamesRatings < b.gamesRatings) ? 1 : ((b.gamesRatings < a.gamesRatings) ? -1 : 0));
+    this.sortMethod = 5;
+  }
+
+  sortByGamesTotalPlayed() {
+    this.users.sort((a, b) => (a.gamesTotalPlayed < b.gamesTotalPlayed) ? 1 : ((b.gamesTotalPlayed < a.gamesTotalPlayed) ? -1 : 0));
+    this.sortMethod = 6;
   }
 
   rowIndex(i) {
